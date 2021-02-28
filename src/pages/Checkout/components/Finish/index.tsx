54100ec823ad2fd61503aeb916b97ab825351ec0
maxleon52 <maxleon522@gmail.com>
2021-02-28 17:31:16 -0300

    chore: add content finish of th modal

--- src/pages/Checkout/components/Finish/index.tsx
+++ src/pages/Checkout/components/Finish/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../../../components/Button';
 
 import { Container } from './styles';
 
@@ -9,9 +10,40 @@ interface Props {
 const Finish: React.FC<Props> = ({ nameComponent }) => {
   return (
     <Container>
-      <button type="button" onClick={() => nameComponent('pronto terminou')}>
-        Finish
-      </button>
+      <header>
+        <h4>CHASE</h4>
+        <img src="" alt="" />
+      </header>
+
+      <form>
+        <label htmlFor="ischeck">
+          <input type="radio" id="ischeck" name="ischeck" value="ischeck" />
+          Checking Account - 6576
+        </label>
+        <label htmlFor="save">
+          <input type="radio" id="save" name="ischeck" value="save" />
+          Savings Account - 6213
+        </label>
+
+        <div>
+          <p>shoes.on Transaction Authorization</p>
+          <p>
+            Today, being 5/6/2019 09:55:37 AM GMT-03, by clicking Continue, I
+            agree to the Terms of Use and authorize shoes.on to use my bank
+            account, as identified below, as a payment method and to debit it
+            via an electronic
+          </p>
+        </div>
+        <span>Printable version</span>
+
+        <Button
+          onClick={() => nameComponent('pronto terminou')}
+          style={{ width: '100%', marginTop: '20px' }}
+          color="#0078cd"
+        >
+          Continue
+        </Button>
+      </form>
     </Container>
   );
 };
--- src/pages/Checkout/components/Finish/styles.ts
+++ src/pages/Checkout/components/Finish/styles.ts
@@ -1,5 +1,117 @@
 import styled from 'styled-components';
+import { Colors } from '../../../../styles/Colors';
 
 export const Container = styled.div`
   color: #000;
+  margin-top: 45px;
+  header {
+    /* border: 2px solid red; */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    font-family: 'Share Tech', sans-serif;
+
+    h4 {
+      font-size: 32px;
+      margin-right: 5px;
+    }
+
+    img {
+      width: 31px;
+      height: 31px;
+      background: #dedede;
+    }
+  }
+
+  form {
+    /* border: 2px solid; */
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 16px;
+
+    label {
+      display: flex;
+      align-items: center;
+      justify-content: baseline;
+      width: 100%;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 19px;
+      letter-spacing: 0em;
+      text-align: left;
+      color: #383535;
+
+      input {
+        margin-right: 10px;
+        width: 15px;
+        height: 15px;
+      }
+    }
+    label + label {
+      margin-top: 7px;
+    }
+
+    > p {
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 19px;
+      letter-spacing: 0em;
+      text-align: left;
+      margin-top: 25px;
+      margin-left: 20px;
+      color: #7c7c7c;
+
+      span {
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+        margin-left: 5px;
+        color: #0078cd;
+        text-decoration: underline;
+      }
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      margin-top: 20px;
+      height: 121px;
+      width: 315px;
+      background: #f4f4f4;
+      padding: 10px;
+
+      P {
+        font-size: 11px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 16px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #7c7c7c;
+      }
+    }
+
+    span {
+      margin-top: 8px;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 19px;
+      letter-spacing: 0em;
+      text-align: left;
+      color: #7c7c7c;
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  }
 `;
