80cc4e769334c5f1eb7482f513fc6e70216f6b86
maxleon52 <maxleon522@gmail.com>
2021-02-28 15:18:19 -0300

    chore: add content message of the modal.

--- src/pages/Checkout/components/Message/index.tsx
+++ src/pages/Checkout/components/Message/index.tsx
@@ -9,9 +9,35 @@ interface Props {
 const Message: React.FC<Props> = ({ nameComponent }) => {
   return (
     <Container>
-      <button type="button" onClick={() => nameComponent('loginBank')}>
-        Message
-      </button>
+      <div>
+        <h4>
+          shoes<b>.on</b>
+        </h4>
+
+        <strong>About signing into</strong>
+        <strong>Online Banking</strong>
+
+        <ul>
+          <li>This is a secure connection to Chase</li>
+          <li>Your bank login cannot be seen by shoes.on</li>
+        </ul>
+
+        <label htmlFor="nomessage">
+          <input
+            type="checkbox"
+            name="nomessage"
+            id="nomessage"
+            style={{ background: 'trasparent' }}
+          />
+          Do not show this message again
+        </label>
+      </div>
+
+      <footer>
+        <button type="button" onClick={() => nameComponent('loginBank')}>
+          OK, got it!
+        </button>
+      </footer>
     </Container>
   );
 };
--- src/pages/Checkout/components/Message/styles.ts
+++ src/pages/Checkout/components/Message/styles.ts
@@ -1,5 +1,141 @@
+import { lighten } from 'polished';
 import styled from 'styled-components';
+import { Colors } from '../../../../styles/Colors';
 
 export const Container = styled.div`
-  color: #000;
+  /* border: 2px solid red; */
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  height: 420px;
+  width: 372px;
+  padding-left: 32px;
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.3) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #788d74;
+  background-blend-mode: overlay, normal;
+  border-radius: 5px 0px 0px 5px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    h4 {
+      font-size: 32px;
+      font-style: normal;
+      font-weight: 900;
+      line-height: 38px;
+      letter-spacing: 0px;
+      text-align: left;
+      margin: 32px 0 11px 0;
+    }
+
+    strong {
+      width: 192px;
+      font-size: 21px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 29px;
+      letter-spacing: 0em;
+      text-align: left;
+    }
+
+    ul {
+      margin-top: 22px;
+      margin-left: 20px;
+      li:first-child {
+        max-width: 200px;
+        list-style: disc;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+      }
+      li:last-child {
+        max-width: 250px;
+        list-style: disc;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+      }
+    }
+
+    label {
+      display: flex;
+      align-items: center;
+      margin-top: 26px;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 16px;
+      letter-spacing: 0em;
+      text-align: left;
+
+      input {
+        width: 14px;
+        height: 14px;
+        margin-right: 10px;
+      }
+      input[type='checkbox']::before {
+        content: ' ';
+        width: inherit;
+        height: inherit;
+        position: absolute;
+        background-color: ${Colors.greenDark};
+        border: 1px solid #fff;
+        border-radius: 2px;
+      }
+      input[type='checkbox']:checked::after {
+        content: ' ';
+        width: 0.2rem;
+        height: 0.5rem;
+        margin-left: 0.3rem;
+        border-color: #fff;
+        border-style: solid;
+        border-width: 0 3px 3px 0;
+        position: absolute;
+        transform: rotate(45deg);
+      }
+    }
+  }
+
+  footer {
+    /* border: 2px solid blue; */
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-bottom: 40px;
+
+    button {
+      background: transparent;
+      padding: 10px 15px;
+      margin-right: 54px;
+      border: 1px solid #ffffff;
+      box-sizing: border-box;
+      border-radius: 3px;
+      color: #ffffff;
+      transition: all 0.2s;
+
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 19px;
+      letter-spacing: 0em;
+      text-align: left;
+
+      :hover {
+        background-color: ${lighten(0.1, Colors.greenDark)};
+      }
+    }
+  }
 `;
